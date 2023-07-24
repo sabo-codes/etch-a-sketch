5d7fc1c19c92ba012d43546f9852c2f48af91501
sabo <124112648+sabo-codes@users.noreply.github.com>
2023-07-23 21:24:00 -0400

    add hover to one square

--- main.js
+++ main.js
@@ -2,6 +2,24 @@ const square = document.createElement('div');
 const container = document.querySelector('.container');
 
 for (let i = 1; i <= 16; i++) {
+    for (let j = 1; j <= 16; j++) {
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('newSquare');
+        container.appendChild(newDiv);
+    }
     let newDiv = document.createElement('div');
+    newDiv.classList.add('newSquare');
     container.appendChild(newDiv);
 }
+
+function hover(element, enter, leave) {
+    element.addEventListener('mouseenter', enter);
+    element.addEventListener('mouseleave', leave);
+}
+
+hover(document.querySelector('.newSquare'), e => {
+    e.target.classList.add('hover')
+}), e => {
+    e.target.classList.remove('hover')
+}
+
