037cd93a5414b818912d7326581e1af69cb55efc
sabo <124112648+sabo-codes@users.noreply.github.com>
2023-07-25 21:19:10 -0400

    test hover for multiple squares

--- main.js
+++ main.js
@@ -12,15 +12,23 @@ for (let i = 1; i <= 16; i++) {
     container.appendChild(newDiv);
 }
 
+// const newSquare = document.querySelectorAll('.newSquare');
+
+// console.log(newSquare)
+// newSquare.forEach(element => element.addEventListener('mouseenter', hover));
+
+// function hover(element) {
+//     element.querySelector('.newSquare').style.backgroundColor = 'black';
+// }
+
 function hover(element, enter, leave) {
-    element.addEventListener('mouseenter', enter);
-    // element.addEventListener('mouseleave', leave);
+    element.addEventListener('mouseover', enter);
+    element.addEventListener('mouseleave', leave);
 }
 
 hover(document.querySelector('.newSquare'), e => {
     e.target.classList.toggle('hover')
-})
-// }), e => {
-//     e.target.classList.remove('hover')
-// }
+}), e => {
+    e.target.classList.remove('hover')
+}
 
