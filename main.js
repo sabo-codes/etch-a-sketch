2b203a6703718619201f20c46bf0e356f5e88bc1
sabo <124112648+sabo-codes@users.noreply.github.com>
2023-07-26 21:20:29 -0400

    add user prompt btn

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const square = document.createElement('div');
 const container = document.querySelector('.container');
+let numOfSides = 0;
 
 for (let i = 1; i <= 16; i++) {
     for (let j = 1; j <= 16; j++) {
@@ -17,3 +18,12 @@ const newSquares = document.querySelectorAll('.newSquare');
 newSquares.forEach(e => e.addEventListener('mouseover', () => e.classList.add('hover')));
 
 newSquares.forEach(e => e.addEventListener('click', () => e.classList.remove('hover')));
+
+
+const promptBtn = document.querySelector('.prompt-btn')
+promptBtn.addEventListener('click', userDecides);
+
+function userDecides () {
+    numOfSides = Number(prompt("Enter number of sides: "));
+    console.log(numOfSides)
+}
