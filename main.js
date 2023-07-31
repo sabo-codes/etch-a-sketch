0add9c00f1c418d3461d48ed4daf434b69ade68a
sabo <124112648+sabo-codes@users.noreply.github.com>
2023-07-30 21:25:54 -0400

    fix hover on start and add clear grid option

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const grid = document.querySelector('.grid');
 
-let mouseDown = false
-document.body.onmousedown = () => (mouseDown = true)
-document.body.onmouseup = () => (mouseDown = false)
+let mouseDown = false;
+document.body.onmousedown = function(){
+    mouseDown = true;
+};
+document.body.onmouseup = function(){
+    mouseDown = false;
+};
 
 function setGridSize(size) {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
@@ -11,25 +15,24 @@ function setGridSize(size) {
     for (let i = 1; i <= size * size; i++) {
         const gridSquare = document.createElement('div');
         gridSquare.classList.add('grid-square');
-        gridSquare.addEventListener('mouseover', hover)
-        gridSquare.addEventListener('mousedown', hover)
+        gridSquare.addEventListener('mouseover', paintGrid);
+        gridSquare.addEventListener('mousedown', paintGrid);
         grid.appendChild(gridSquare);
     }
 }
 
-function hover(e) {
-    if (e.type === 'mouseover' && mouseDown) {
-    e.target.style.backgroundColor = "black";
-    console.log(e.type);
-    console.log(e.target)
-    } else {
+function paintGrid(e) {
+    if (!mouseDown && e.type === 'mouseover') {
         return;
+    } else {
+        e.target.style.backgroundColor = "black";
+        console.log(mouseDown)
     }
 }
 
 setGridSize(16);
 
-document.querySelector('button').addEventListener('click', changeGridSize);
+document.querySelector('.submit').addEventListener('click', changeGridSize);
 
 
 function changeGridSize () {
@@ -43,3 +46,13 @@ function changeGridSize () {
         setGridSize(gridInput);
     }
 }
+
+function clearGrid() {
+    const paintedSquares = document.querySelectorAll('.grid-square');
+
+    paintedSquares.forEach(e => e.style.backgroundColor = "white");
+}
+
+const clear = document.querySelector('.clear-btn');
+clear.addEventListener('click', clearGrid);
+
